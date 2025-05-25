e9d814ec9adde93d3cba69e478954642dd7d4a51
TaeChanathip <chanathip.pla@builk.com>
2025-05-25 20:59:05 +0700

    print

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,14 @@
 #include <Arduino.h>
-#include <WiFi.h>
+#include <ArduinoJson.h>
 #include "dht_sensor.h"
 #include "soil_sensor.h"
 #include "water_temp_sensor.h"
 #include "weight_sensor.h"
-#include "mqtt_service.h"
-#include "config.h"
 #include "acs712_sensor.h"
 #include "voltage_sensor.h"
 
-bool connectToWiFi() {
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print("Connecting to WiFi");
-    
-    int attempts = 0;
-    while (WiFi.status() != WL_CONNECTED && attempts < 20) {
-        delay(500);
-        Serial.print(".");
-        attempts++;
-    }
-    
-    if (WiFi.status() == WL_CONNECTED) {
-        Serial.println("\nConnected to WiFi");
-        Serial.print("IP address: ");
-        Serial.println(WiFi.localIP());
-        return true;
-    }
-    Serial.println("\nFailed to connect to WiFi");
-    return false;
-}
-
 void setup() {
-  Serial.begin(115200);
-  
-  // Connect to WiFi first
-  if (!connectToWiFi()) {
-    Serial.println("Failed to initialize WiFi. System may not work properly.");
-  }
+  Serial.begin(9600);
   
   initDHT();
   initSoil();
@@ -44,22 +16,53 @@ void setup() {
   initWeight();
   initACS712();
   initVoltageSensor();
-  mqttService.init();
 }
 
 void loop() {
-  // Read and publish data for each sensor
-  mqttService.publishSensorData(readDHTSystem());
-  mqttService.publishSensorData(readDHTFeeder());
-  mqttService.publishSensorData(readSoil());
-  mqttService.publishSensorData(readWaterTemp());
-  mqttService.publishSensorData(readWeight());
-  mqttService.publishSensorData(readACS712());
-  mqttService.publishSensorData(readVoltageSensor());
+  // Read and send data from each sensor
   
-  // Handle MQTT loop
-  mqttService.loop();
-  
+  // DHT System
+  StaticJsonDocument<256> dhtSystem = readDHTSystem();
+  String jsonString;
+  serializeJson(dhtSystem, jsonString);
+  Serial.println(jsonString);
+
+  // DHT Feeder
+  StaticJsonDocument<256> dhtFeeder = readDHTFeeder();
+  jsonString = "";
+  serializeJson(dhtFeeder, jsonString);
+  Serial.println(jsonString);
+
+  // Soil Sensor
+  StaticJsonDocument<256> soil = readSoil();
+  jsonString = "";
+  serializeJson(soil, jsonString);
+  Serial.println(jsonString);
+
+  // Water Temperature
+  StaticJsonDocument<256> waterTemp = readWaterTemp();
+  jsonString = "";
+  serializeJson(waterTemp, jsonString);
+  Serial.println(jsonString);
+
+  // Weight Sensor
+  StaticJsonDocument<256> weight = readWeight();
+  jsonString = "";
+  serializeJson(weight, jsonString);
+  Serial.println(jsonString);
+
+  // Current Sensor
+  StaticJsonDocument<256> current = readACS712();
+  jsonString = "";
+  serializeJson(current, jsonString);
+  Serial.println(jsonString);
+
+  // Voltage Sensor
+  StaticJsonDocument<256> voltage = readVoltageSensor();
+  jsonString = "";
+  serializeJson(voltage, jsonString);
+  Serial.println(jsonString);
+
   // Wait before next reading
   delay(5000);  // 5 seconds delay
 }
