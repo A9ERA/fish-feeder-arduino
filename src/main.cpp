86949d4647f32493216c6db5eae920dfd6a28519
TaeChanathip <chanathip.pla@builk.com>
2025-05-25 21:19:09 +0700

    add flag send

--- src/main.cpp
+++ src/main.cpp
@@ -21,47 +21,48 @@ void setup() {
 void loop() {
   // Read and send data from each sensor
   
-  // DHT System
-  StaticJsonDocument<256> dhtSystem = readDHTSystem();
   String jsonString;
-  serializeJson(dhtSystem, jsonString);
-  Serial.println(jsonString);
+  
+  // // DHT System
+  // StaticJsonDocument<256> dhtSystem = readDHTSystem();
+  // serializeJson(dhtSystem, jsonString);
+  // Serial.println("[SEND] - " + jsonString);
 
   // DHT Feeder
   StaticJsonDocument<256> dhtFeeder = readDHTFeeder();
   jsonString = "";
   serializeJson(dhtFeeder, jsonString);
-  Serial.println(jsonString);
+  Serial.println("[SEND] - " + jsonString);
 
-  // Soil Sensor
-  StaticJsonDocument<256> soil = readSoil();
-  jsonString = "";
-  serializeJson(soil, jsonString);
-  Serial.println(jsonString);
+  // // Soil Sensor
+  // StaticJsonDocument<256> soil = readSoil();
+  // jsonString = "";
+  // serializeJson(soil, jsonString);
+  // Serial.println(jsonString);
 
-  // Water Temperature
-  StaticJsonDocument<256> waterTemp = readWaterTemp();
-  jsonString = "";
-  serializeJson(waterTemp, jsonString);
-  Serial.println(jsonString);
+  // // Water Temperature
+  // StaticJsonDocument<256> waterTemp = readWaterTemp();
+  // jsonString = "";
+  // serializeJson(waterTemp, jsonString);
+  // Serial.println(jsonString);
 
-  // Weight Sensor
-  StaticJsonDocument<256> weight = readWeight();
-  jsonString = "";
-  serializeJson(weight, jsonString);
-  Serial.println(jsonString);
+  // // Weight Sensor
+  // StaticJsonDocument<256> weight = readWeight();
+  // jsonString = "";
+  // serializeJson(weight, jsonString);
+  // Serial.println(jsonString);
 
-  // Current Sensor
-  StaticJsonDocument<256> current = readACS712();
-  jsonString = "";
-  serializeJson(current, jsonString);
-  Serial.println(jsonString);
+  // // Current Sensor
+  // StaticJsonDocument<256> current = readACS712();
+  // jsonString = "";
+  // serializeJson(current, jsonString);
+  // Serial.println(jsonString);
 
-  // Voltage Sensor
-  StaticJsonDocument<256> voltage = readVoltageSensor();
-  jsonString = "";
-  serializeJson(voltage, jsonString);
-  Serial.println(jsonString);
+  // // Voltage Sensor
+  // StaticJsonDocument<256> voltage = readVoltageSensor();
+  // jsonString = "";
+  // serializeJson(voltage, jsonString);
+  // Serial.println(jsonString);
 
   // Wait before next reading
   delay(5000);  // 5 seconds delay
